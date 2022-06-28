4c0b334c0c959e09aae2ca92db9498ce671c871b
weirui88888 <ruiwei88888@163.com>
2022-06-28 19:17:21 +0800

    feat: 🎸 add commonMark for get code and strong
    \

--- src/util.js
+++ src/util.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const chalk = require("chalk")
 const log = console.log
+const commonMark = require("commonMark")
 
 const greenLog = (message) => {
   log(chalk.green(message))
@@ -21,9 +22,26 @@ const pickKeywords = (filePath) => {
   if (!fs.existsSync(filePath)) {
     return redLog(`${filePath} does not exist, please confirm and execute again `)
   }
-  const mdContent = fs.readFileSync(filePath, { encoding: "utf8" })
-  console.log(mdContent)
-  // return mdContent
+  const markdown = fs.readFileSync(filePath, { encoding: "utf8" })
+  let parsed = new commonMark.Parser().parse(markdown)
+  let walker = parsed.walker()
+  let event
+  let nodeList = []
+  let st = []
+  while ((event = walker.next())) {
+    let node = event.node
+    if (node.type === "code" && node.literal) {
+      nodeList.push(node)
+    }
+    if (node.type === "strong" && node.firstChild._literal) {
+      st.push(node)
+    }
+  }
+
+  const literals = nodeList.map((node) => node.literal)
+  const sts = st.map((node) => node.firstChild.literal)
+  const all = [...new Set([...literals, ...sts])]
+  console.log(all)
 }
 
 exports.getUserConfig = getUserConfig
