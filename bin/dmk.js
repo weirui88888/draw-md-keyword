e3c407e8a85388f19b7f2b3f96b6ec68858782d0
weirui88888 <ruiwei88888@163.com>
2022-06-29 22:43:46 +0800

    :tada:give tips by without dwk init before dwk draw

--- bin/dmk.js
+++ bin/dmk.js
@@ -3,10 +3,11 @@
 const { Command } = require('commander')
 const path = require('path')
 const pkg = require('../package.json')
+const fs = require('fs')
 const defaultConfig = require('./default.config')
 const Generator = require('../src/generator')
 const { init } = require('../src/init')
-const { getUserConfig, pickKeywords } = require('../src/util')
+const { getUserConfig, pickKeywords, redLog } = require('../src/util')
 
 const userDir = process.cwd()
 const program = new Command()
@@ -25,6 +26,9 @@ program
   .description('Randomly output cloud images based on your input file')
   .argument('<filePath>', 'Keyword cloud map will be automatically generated according to the configuration file')
   .action(filePath => {
+    if (!fs.existsSync(userConfigPath)) {
+      return redLog(`please run 'dmk init' to initialize a config file before use [dwk draw]`)
+    }
     const keywords = pickKeywords(path.resolve(userDir, filePath))
     new Generator(keywords, getUserConfig(userConfigPath)).draw()
   })
